06438d8ce9fa9b7747ae98d67448b79107a3b56f
Anthony J. DeMeglio <22527727+ademeglio@users.noreply.github.com>
2018-07-03 12:25:03 -0400

    remove unneeded method

--- src/main/java/org/wecancodeit/courses/TextBookRepository.java
+++ src/main/java/org/wecancodeit/courses/TextBookRepository.java
@@ -1,10 +1,8 @@
 package org.wecancodeit.courses;
 
-import java.util.Collection;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TextbookRepository extends CrudRepository<Textbook, Long> {
 	
-	Collection<Textbook> findByCourseContains(Course course);
 	
 }
