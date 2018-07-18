c4e4f5394fcca50d6e479cd6051f1c166f1a3778
Anthony J. DeMeglio <22527727+ademeglio@users.noreply.github.com>
2018-07-18 11:38:17 -0400

    finish ajax lesson

---
+++ src/main/resources/static/js/courses-ajax-example.js
@@ -0,0 +1,35 @@
+const xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+        const res = JSON.parse(xhr.response);
+        const container = document.querySelector('.container');
+
+        console.log({res});
+
+        res.forEach(function(course){
+            const courseItem = document.createElement('div');
+
+            const name = document.createElement('h2');
+            name.innerText = course.name;
+            
+            const description = document.createElement('p');
+            description.innerText = course.description;
+            
+            const topics = [];
+            course.topicUrls.forEach(topicUrl => {
+            	const topicUrlElement = document.createElement('li');
+            	topicUrlElement.innerHTML = `Course Topics: <a href="${topicUrl}">${topicUrl}</a>`;
+            	topics.push(topicUrlElement);
+            })
+
+            container.appendChild(courseItem);
+            courseItem.appendChild(name);
+            courseItem.appendChild(description);
+            
+            topics.forEach(topicUrl => courseItem.appendChild(topicUrl));
+        });
+    }
+}
+
+xhr.open('GET', 'http://localhost:8080/courses', true);
+xhr.send();
